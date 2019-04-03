64a86c000adf21d72b84e7138aa2733c77350008
MigaraArcher <evamarcod@gmail.com>
2019-04-03 23:35:55 +0200

    MapfilterReduce

--- MapFilterReduce.py
+++ MapFilterReduce.py
@@ -1,4 +1,7 @@
+from functools import reduce
 lista = [1, 3, -1, 15, 9]
 
 listadobles = map(lambda x: x*2, lista)
 listapares = filter(lambda x: x % 2 == 0, lista)
+
+sumatodo = reduce(lambda x, y: x + y, lista)
