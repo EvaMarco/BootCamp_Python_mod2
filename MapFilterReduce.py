de74bb7d1caace4c1ad595e7144eaf71e2a640ce
MigaraArcher <evamarcod@gmail.com>
2019-04-08 16:32:12 +0200

    map, filter reduce

--- MapFilterReduce.py
+++ MapFilterReduce.py
@@ -5,3 +5,9 @@ listadobles = map(lambda x: x*2, lista)
 listapares = filter(lambda x: x % 2 == 0, lista)
 
 sumatodo = reduce(lambda x, y: x + y, lista)
+suma100 = reduce(lambda x, y: x+y, range(101))
+
+print(list(listapares))
+print(list(listadobles))
+print(sumatodo)
+print(suma100)
