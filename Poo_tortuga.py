7da9f2815804612197881e1a459f1e8650a5d672
MigaraArcher <evamarcod@gmail.com>
2019-04-10 12:46:29 +0200

    Ligera modificación en tortuga

--- Poo_tortuga.py
+++ Poo_tortuga.py
@@ -1,10 +1,16 @@
 import turtle
 
 tortu = turtle.Turtle()
-otratortu=turtle.Turtle()
+otratortu = turtle.Turtle()
+lenti = turtle.Turtle()
+
 tortu.shape('turtle')
 tortu.fd(50)
 
 otratortu.color('green')
 otratortu.left(90)
 otratortu.fd(50)
+
+lenti.speed(1)
+tortu.speed(5)
+otratortu.speed(7)
