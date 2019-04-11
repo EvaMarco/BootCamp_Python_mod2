6652063d43c58ca886ec3126a55580eb4cc23d7c
MigaraArcher <evamarcod@gmail.com>
2019-04-11 12:49:53 +0200

    Cambios en nuestra clase termometro

--- Poo_Conversor.py
+++ Poo_Conversor.py
@@ -1,23 +1,20 @@
 class Termometro:
+    # Función constructora.
+    
     def __init__(self):
+        # Atributos iniciales. Son privados, no se pueden consultar.
         self.__unidad = 'C'
         self.__temperatura = 0
 
-    def __conversor(self, temperatura, unidad):
-        if unidad == 'C':
-            return '{}ºF'.format(int(temperatura) * 9/5 + 32)
-        elif unidad == 'F':
-            return '{}º C.'.format(int(temperatura - 32) * (5/9))
-        else:
-            return 'Unidad incorrecta'
-
+    # La función que informa del estado, la podemos usar en el print.
     def __str__(self):
         return '{}º{}.'.format(self.__temperatura, self.__unidad)
 
 
-# Vamos a hacer un metodo setter y getter para las unidades y otro para las temperatuas.
-    def unidadmedida(self, uni = None):
+    # Vamos a hacer un metodo setter y getter para las unidades y otro para las temperatuas.
 
+    def unidadmedida(self, uni = None):
+    # Como los atributos son privados con esta función podemos consultarlos.
         if uni == None:
 
             return 'Las unidades son {}.'.format(self.__unidad)
@@ -31,10 +28,24 @@ class Termometro:
             return '{}º.'.format(self.__temperatura)
         else:
             self.__temperatura = temperatura
-   
-    def mide(self, unim=None):
 
+    # La función que nos cambia de unidades. Es privada porque no nos interesa que pueda ser llamada desde fuera.
+    def __conversor(self, temperatura, unidad):
+        if unidad == 'C':
+            return '{}ºF'.format(int(temperatura) * 9/5 + 32)
+        elif unidad == 'F':
+            return '{}º C.'.format(int(temperatura - 32) * (5/9))
+        else:
+            return 'Unidad incorrecta'
+        
+    # Esta es la funcionalidad del termometro.
+    
+    def mide(self, unim=None):
+    
         if unim == None or unim == self.__unidad:
             return self.__str__()
         else:
-            return self.__conversor(self.__temperatura, self.__unidad)
+            if unim == 'F' or unim == 'C'
+                return self.__conversor(self.__temperatura, self.__unidad)
+            else:
+                return self.__str__()
